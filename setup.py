3565a15823348964eafb0348393d53eef4bc3489
jie.xin <jie.xin@avanade.com>
2024-02-01 12:09:49 +0100

    ready for aep profiles kpi

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-"""
-setup.py configuration script describing how to build and package this project.
-
-This file is primarily used by the setuptools library and typically should not
-be executed directly. See README.md for how to deploy, test, and run
-the aep_profiles_kpi project.
-"""
 from setuptools import setup, find_packages
 
 import sys
@@ -18,8 +11,7 @@ setup(
     # We use timestamp as Local version identifier (https://peps.python.org/pep-0440/#local-version-identifiers.)
     # to ensure that changes to wheel package are picked up when used on all-purpose clusters
     version=aep_profiles_kpi.__version__ + "+" + datetime.datetime.utcnow().strftime("%Y%m%d.%H%M%S"),
-    url="https://databricks.com",
-    author="jie.xin@avanade.com",
+    author="Henkel",
     description="wheel file based on aep_profiles_kpi/src",
     packages=find_packages(where='./src'),
     package_dir={'': 'src'},
@@ -29,9 +21,7 @@ setup(
         ]
     },
     install_requires=[
-        # Dependencies in case the output wheel file is used as a library dependency.
-        # For defining dependencies, when this package is used in Databricks, see:
-        # https://docs.databricks.com/dev-tools/bundles/library-dependencies.html
+        "aepp"
         "setuptools"
     ],
 )
